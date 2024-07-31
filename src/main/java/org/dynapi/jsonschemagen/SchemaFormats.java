86fbc503627749749b80706545ac5860fff24681
playerg9 <g9yout@gmail.com>
2024-07-31 17:25:29 +0200

    added some enum classes

---
+++ src/main/java/org/dynapi/jsonschemagen/SchemaFormats.java
@@ -0,0 +1,31 @@
+package org.dynapi.jsonschemagen;
+
+public class SchemaFormats {
+    // Dates and Times
+    public static final String DATE_TIME = "date-time";
+    public static final String TIME = "time";
+    public static final String DATE = "date";
+    public static final String DURATION = "duration";
+    // Email Addresses
+    public static final String EMAIL = "email";
+    public static final String IDN_EMAIL = "idn-email";
+    // Hostnames
+    public static final String HOSTNAME = "hostname";
+    public static final String IDN_HOSTNAME = "idn-hostname";
+    // IP Addresses
+    public static final String IPV4 = "ipv4";
+    public static final String IPV6 = "ipv6";
+    // Resource Identifiers
+    public static final String UUID = "uuid";
+    public static final String URI = "uri";
+    public static final String URI_REFERENCE = "uri-reference";
+    public static final String IRI = "iri";
+    public static final String IRI_REFERENCE = "iri-reference";
+    // URI Template
+    public static final String URI_TEMPLATE = "uri-template";
+    // JSON Pointer
+    public static final String JSON_POINTER = "json-pointer";
+    public static final String RELATIVE_JSON_POINTER = "relative-json-pointer";
+    // Regular Expression
+    public static final String REGEX = "regex";
+}
---
+++ src/main/java/org/dynapi/jsonschemagen/SchemaTypes.java
@@ -0,0 +1,11 @@
+package org.dynapi.jsonschemagen;
+
+public class SchemaTypes {
+    public static final String NULL = "null";
+    public static final String BOOLEAN = "boolean";
+    public static final String INTEGER = "integer";
+    public static final String NUMBER = "number";
+    public static final String STRING = "string";
+    public static final String OBJECT = "object";
+    public static final String ARRAY = "array";
+}
