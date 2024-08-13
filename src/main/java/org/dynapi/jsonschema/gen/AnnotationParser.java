85b69e7ca16bb52505ea547e595b57a23d30bc2d
playerg9 <g9yout@gmail.com>
2024-08-13 18:22:55 +0200

    moved JsonSchemaGenerator methods to AnnotationParser and used schema classes

---
+++ src/main/java/org/dynapi/jsonschema/gen/AnnotationParser.java
@@ -0,0 +1,181 @@
+package org.dynapi.jsonschema.gen;
+
+import org.dynapi.jsonschema.gen.annotations.*;
+import org.dynapi.jsonschema.gen.schema.*;
+
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.Stream;
+
+class AnnotationParser {
+    protected static Schema<?, ?> generateJsonSchemaForObject(StateData state, Class<?> clazz) {
+        String refName = clazz.getCanonicalName();
+        JRef objectRef = new JRef(refName);
+        // we always return a ref for an object for recursive references
+        if (state.references.containsKey(clazz.getCanonicalName())) {
+            return objectRef;
+        }
+
+        JObject jsonSchema = new JObject()
+                .allowAdditionalProperties(clazz.isAnnotationPresent(AllowAdditionalProperties.class));
+        state.references.put(refName, jsonSchema);
+        applyDescription(clazz.getAnnotation(Description.class), jsonSchema);
+
+        List<String> fieldNames = Arrays.stream(clazz.getDeclaredFields()).map(Field::getName).toList();
+
+        HiddenProperties hiddenProperties = clazz.getAnnotation(HiddenProperties.class);
+        List<String> hiddenPropertiesNames = List.of(hiddenProperties != null ? hiddenProperties.value() : new String[0]);
+
+        List<String> diff = new ArrayList<>(hiddenPropertiesNames);
+        diff.removeAll(fieldNames);
+        if (!diff.isEmpty()) {
+            throw new RuntimeException("Unknown properties to hide: " + String.join(", ", diff));
+        }
+
+        for (Field field : clazz.getDeclaredFields()) {
+            if (hiddenPropertiesNames.contains(field.getName()) || field.isAnnotationPresent(Hidden.class)) continue;
+
+            Schema<?, ?> fieldSchema = generateJsonSchemaForField(state, field.getType());
+
+            applyDescription(field.getAnnotation(Description.class), fieldSchema);
+            applyExamples(field.getAnnotation(Examples.class), fieldSchema);
+            applyConstraints(field.getAnnotation(Constraints.class), fieldSchema);
+
+            if (field.isAnnotationPresent(Deprecated.class))
+                fieldSchema.deprecated();
+
+            if (field.isAnnotationPresent(Required.class))
+                jsonSchema.required(field.getName());
+
+            RequiredIf requiredIf = field.getAnnotation(RequiredIf.class);
+            if (requiredIf != null) {
+                String ifField = field.getName();
+                if (!fieldNames.contains(ifField)) {
+                    String errorMessage = String.format("Bad @RequiredIf(\"%s\") of property %s of class %s", ifField, field.getName(), clazz.getCanonicalName());
+                    throw new RuntimeException(errorMessage);
+                }
+                jsonSchema.requiredIf(ifField, requiredIf.value());
+            }
+
+            jsonSchema.addProperty(field.getName(), fieldSchema);
+        }
+
+        return jsonSchema;
+    }
+
+    protected static Schema<?, ?> generateJsonSchemaForField(StateData state, Class<?> clazz) {
+        if (clazz.equals(int.class) || clazz.equals(Integer.class)) {
+            return new JInteger();
+        } else if (clazz.equals(long.class) || clazz.equals(Long.class)) {
+            return new JInteger();
+        } else if (clazz.equals(double.class) || clazz.equals(Double.class)) {
+            return new JNumber();
+        } else if (clazz.equals(float.class) || clazz.equals(Float.class)) {
+            return new JNumber();
+        } else if (clazz.equals(boolean.class) || clazz.equals(Boolean.class)) {
+            return new JBoolean();
+        } else if (clazz.equals(String.class)) {
+            return new JString();
+        } else if (clazz.equals(Map.class)) {
+            return new JObject();
+        } else if (clazz.equals(Object.class)) {
+            return new JAnyType();
+        } else if (clazz.isArray()) {  // todo: support for List?
+            return generateJsonSchemaForArrayField(state, clazz);
+        } else if (clazz.isEnum()) {
+            return generateJsonSchemaForEnum(clazz);
+        } else {
+            return generateJsonSchemaForObject(state, clazz);
+        }
+    }
+
+    protected static Schema<?, ?> generateJsonSchemaForArrayField(StateData state, Class<?> clazz) {
+        return new JArray()
+                .addType(generateJsonSchemaForField(state, clazz.getComponentType()));
+    }
+
+    protected static JString generateJsonSchemaForEnum(Class<?> clazz) {
+        return new JString()
+                .options(Stream.of(clazz.getEnumConstants()).map(v -> ((Enum<?>) v).name()).toList());
+    }
+
+    protected static void applyDescription(Description meta, Schema<?, ?> object) {
+        if (meta == null) return;
+        object.description(meta.value());
+    }
+
+    protected static <T> void applyExamples(Examples examples, Schema<?, T> object) {
+        if (examples == null) return;
+        //noinspection unchecked
+        object.examples((List<T>) Arrays.stream(examples.value()).map(Util::parseStringToJsonX).toList());
+    }
+
+    protected static <T> void applyConstraints(Constraints constraints, Schema<?, T> schema) {
+        if (constraints == null) return;
+
+        // common constraints
+
+        if (!constraints.option().isEmpty())
+            //noinspection unchecked
+            schema.constValue((T) constraints.option());
+        if (constraints.options().length > 0)
+            //noinspection unchecked
+            schema.options((List<T>) Arrays.stream(constraints.options()).map(Util::parseStringToJsonX).toList());
+
+        // string constrains
+
+        if (schema instanceof JString jString) {
+            if (constraints.minLength() != -1)
+                jString.minLength(constraints.minLength());
+            if (constraints.maxLength() != -1)
+                jString.maxLength(constraints.maxLength());
+
+            if (!constraints.pattern().isEmpty())
+                jString.pattern(constraints.pattern());
+
+            if (!constraints.format().isEmpty())
+                jString.format(constraints.format());
+        }
+
+        // numeric constraints
+
+        if (schema instanceof JNumber jNumber) {
+            if (constraints.multipleOf() != -1)
+                jNumber.multipleOf(constraints.multipleOf());
+
+            if (constraints.ge() != -1)
+                jNumber.gte(constraints.ge());
+            if (constraints.gt() != -1)
+                jNumber.gt(constraints.gt());
+            if (constraints.le() != -1)
+                jNumber.lte(constraints.le());
+            if (constraints.lt() != -1)
+                jNumber.lt(constraints.lt());
+        }
+
+        // object constraints
+
+        if (schema instanceof JObject jObject) {
+            if (constraints.minProperties() != -1)
+                jObject.minProperties(constraints.minProperties());
+            if (constraints.maxProperties() != -1)
+                jObject.maxProperties(constraints.maxProperties());
+        }
+
+        // array constraints
+
+        if (schema instanceof JArray jArray) {
+            if (constraints.minItems() != -1)
+                jArray.minSize(constraints.minItems());
+            if (constraints.maxItems() != -1)
+                jArray.maxSize(constraints.maxItems());
+
+            if (constraints.uniqueItems())
+                jArray.uniqueItems();
+        }
+    }
+
+    protected static class StateData {
+        protected Map<String, Schema<?, ?>> references = new HashMap<>();
+    }
+}
--- src/main/java/org/dynapi/jsonschema/gen/JsonSchemaGenerator.java
+++ src/main/java/org/dynapi/jsonschema/gen/JsonSchemaGenerator.java
@@ -1,14 +1,12 @@
 package org.dynapi.jsonschema.gen;
 
-import org.dynapi.jsonschema.gen.annotations.*;
-import org.dynapi.jsonschema.gen.annotations.JsonSchemaAble;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.lang.reflect.Field;
-import java.util.*;
-
 public class JsonSchemaGenerator {
+    public static JsonSchemaBuilder builder() {
+        return new JsonSchemaBuilder();
+    }
+
     /**
      * converts a class to json-schema
      *
@@ -16,26 +14,16 @@ public class JsonSchemaGenerator {
      * @return json-schema
      */
     public static JSONObject generateJsonSchema(Class<?> clazz) {
-        if (!clazz.isAnnotationPresent(JsonSchemaAble.class))
-            throw new RuntimeException(String.format("%s is not marked as @JsonSchemaAble", clazz.getCanonicalName()));
+        AnnotationParser.StateData state = new AnnotationParser.StateData();
 
-        JSONObject jsonSchema = new JSONObject();
-        jsonSchema.put("$schema", "https://json-schema.org/draft/2020-12/schema");
-        jsonSchema.put("title", clazz.getSimpleName());
+        JsonSchemaBuilder builder = new JsonSchemaBuilder()
+                .title(clazz.getCanonicalName())
+                .schema(AnnotationParser.generateJsonSchemaForObject(state, clazz));
 
-        StateData state = new StateData();
+        for (var ref : state.references.entrySet())
+            builder.addDefinition(ref.getKey(), ref.getValue());
 
-        JSONObject objectSchema = generateJsonSchemaForObject(state, clazz);
-        for (String key : objectSchema.keySet())
-            jsonSchema.put(key, objectSchema.get(key));
-
-        JSONObject definitions = new JSONObject();
-        jsonSchema.put("$defs", definitions);
-        for (var ref : state.references.entrySet()) {
-            definitions.put(ref.getKey().getCanonicalName(), ref.getValue());
-        }
-
-        return jsonSchema;
+        return builder.build();
     }
 
     /**
@@ -47,201 +35,5 @@ public class JsonSchemaGenerator {
     public static String generateJsonSchemaAsString(Class<?> clazz) {
         JSONObject jsonSchema = generateJsonSchema(clazz);
         return jsonSchema.toString(2);
-    }
-
-    private static JSONObject generateJsonSchemaForObject(StateData state, Class<?> clazz) {
-        // we always return a ref for an object for recursive references
-        JSONObject jsonSchema = new JSONObject()
-                .put("$ref", "#/$defs/" + clazz.getCanonicalName());
-        if (state.references.containsKey(clazz)) {
-            return jsonSchema;
-        }
-
-        JSONObject refJsonSchema = new JSONObject();
-        state.references.put(clazz, refJsonSchema);
-        refJsonSchema.put("type", "object");
-        refJsonSchema.put("additionalProperties", clazz.isAnnotationPresent(AllowAdditionalProperties.class));
-        applyDescription(clazz.getAnnotation(Description.class), refJsonSchema);
-
-        List<String> fieldNames = Arrays.stream(clazz.getDeclaredFields()).map(Field::getName).toList();
-
-        HiddenProperties hiddenProperties = clazz.getAnnotation(HiddenProperties.class);
-        List<String> hiddenPropertiesNames = List.of(hiddenProperties != null ? hiddenProperties.value() : new String[0]);
-
-        List<String> diff = new ArrayList<>(hiddenPropertiesNames);
-        diff.removeAll(fieldNames);
-        if (!diff.isEmpty()) {
-            throw new RuntimeException("Unknown properties to hide: " + String.join(", ", diff));
-        }
-
-        JSONArray required = new JSONArray();
-        JSONObject dependentRequired = new JSONObject();
-
-        JSONObject properties = new JSONObject();
-        for (Field field : clazz.getDeclaredFields()) {
-            if (hiddenPropertiesNames.contains(field.getName()) || field.isAnnotationPresent(Hidden.class)) continue;
-
-            JSONObject fieldSchema = generateJsonSchemaForField(state, field.getType());
-
-            applyDescription(field.getAnnotation(Description.class), fieldSchema);
-            applyExamples(field.getAnnotation(Examples.class), fieldSchema);
-            applyConstraints(field.getAnnotation(Constraints.class), fieldSchema);
-
-            if (field.isAnnotationPresent(Deprecated.class))
-                fieldSchema.put("deprecated", true);
-
-            // "required": ["field-name"]
-            if (field.isAnnotationPresent(Required.class))
-                required.put(field.getName());
-
-            // "dependentRequired": { "ifField": ["thenField1", "thenField2"] }
-            RequiredIf requiredIf = field.getAnnotation(RequiredIf.class);
-            if (requiredIf != null) {
-                String ifField = requiredIf.value();
-                if (!fieldNames.contains(ifField)) {
-                    String errorMessage = String.format("Bad @RequiredIf(\"%s\") of property %s of class %s", ifField, field.getName(), clazz.getCanonicalName());
-                    throw new RuntimeException(errorMessage);
-                } else if (dependentRequired.has(ifField))
-                    dependentRequired.getJSONArray(ifField).put(field.getName());
-                else
-                    dependentRequired.put(ifField, new JSONArray().put(field.getName()));
-            }
-
-            properties.put(field.getName(), fieldSchema);
-        }
-        refJsonSchema.put("properties", properties);
-
-        if (!required.isEmpty())
-            refJsonSchema.put("required", required);
-
-        if (!dependentRequired.isEmpty())
-            refJsonSchema.put("dependentRequired", dependentRequired);
-
-        return jsonSchema;
-    }
-
-    private static JSONObject generateJsonSchemaForField(StateData state, Class<?> clazz) {
-        JSONObject jsonSchema = new JSONObject();
-
-        if (clazz.equals(int.class) || clazz.equals(Integer.class)) {
-            jsonSchema.put("type", SchemaTypes.INTEGER);
-        } else if (clazz.equals(long.class) || clazz.equals(Long.class)) {
-            jsonSchema.put("type", SchemaTypes.INTEGER);
-        } else if (clazz.equals(double.class) || clazz.equals(Double.class)) {
-            jsonSchema.put("type", SchemaTypes.NUMBER);
-        } else if (clazz.equals(float.class) || clazz.equals(Float.class)) {
-            jsonSchema.put("type", SchemaTypes.NUMBER);
-        } else if (clazz.equals(boolean.class) || clazz.equals(Boolean.class)) {
-            jsonSchema.put("type", SchemaTypes.BOOLEAN);
-        } else if (clazz.equals(String.class)) {
-            jsonSchema.put("type", SchemaTypes.STRING);
-        } else if (clazz.equals(Map.class)) {
-            jsonSchema.put("type", SchemaTypes.OBJECT);
-        } else if (clazz.equals(Object.class)) {
-            // todo: verify if missing type == any
-        } else if (clazz.isArray()) {  // todo: support for List?
-            JSONObject objectSchema = generateJsonSchemaForArrayField(state, clazz);
-            for (String key : objectSchema.keySet())
-                jsonSchema.put(key, objectSchema.get(key));
-        } else if (clazz.isEnum()) {
-            JSONObject enumSchema = generateJsonSchemaForEnum(clazz);
-            for (String key : enumSchema.keySet())
-                jsonSchema.put(key, enumSchema.get(key));
-        } else {
-            JSONObject objectSchema = generateJsonSchemaForObject(state, clazz);
-            for (String key : objectSchema.keySet())
-                jsonSchema.put(key, objectSchema.get(key));
-        }
-        return jsonSchema;
-    }
-
-    private static JSONObject generateJsonSchemaForArrayField(StateData state, Class<?> clazz) {
-        JSONObject jsonSchema = new JSONObject();
-        jsonSchema.put("type", "array");
-
-        JSONObject items = new JSONObject();
-        jsonSchema.put("items", items);
-
-        JSONObject itemType = generateJsonSchemaForField(state, clazz.getComponentType());
-        for (String key : itemType.keySet())
-            items.put(key, itemType.get(key));
-
-        return jsonSchema;
-    }
-
-    private static JSONObject generateJsonSchemaForEnum(Class<?> clazz) {
-        JSONObject jsonSchema = new JSONObject();
-        jsonSchema.put("type", SchemaTypes.STRING);
-        jsonSchema.put("enum", List.of(clazz.getEnumConstants()));
-        return jsonSchema;
-    }
-
-    private static void applyDescription(Description meta, JSONObject object) {
-        if (meta == null) return;
-        object.put("description", meta.value());
-    }
-
-    private static void applyExamples(Examples examples, JSONObject object) {
-        if (examples == null) return;
-        object.put("examples", Arrays.stream(examples.value()).map(Util::parseStringToJsonX).toList());
-    }
-
-    private static void applyConstraints(Constraints constraints, JSONObject object) {
-        if (constraints == null) return;
-
-        // common constraints
-
-        if (!constraints.option().isEmpty())
-            object.put("const", new JSONObject(constraints.option()));
-        if (constraints.options().length > 0)
-            object.put("enum", Arrays.stream(constraints.options()).map(Util::parseStringToJsonX).toList());
-
-        // string constrains
-
-        if (constraints.minLength() != -1)
-            object.put("minLength", constraints.minLength());
-        if (constraints.maxLength() != -1)
-            object.put("maxLength", constraints.maxLength());
-
-        if (!constraints.pattern().isEmpty())
-            object.put("pattern", constraints.pattern());
-
-        if (!constraints.format().isEmpty())
-            object.put("format", constraints.format());
-
-        // numeric constraints
-
-        if (constraints.multipleOf() != -1)
-            object.put("multipleOf", constraints.multipleOf());
-
-        if (constraints.ge() != -1)
-            object.put("minimum", constraints.ge());
-        if (constraints.gt() != -1)
-            object.put("exclusiveMinimum", constraints.gt());
-        if (constraints.le() != -1)
-            object.put("maximum", constraints.le());
-        if (constraints.lt() != -1)
-            object.put("exclusiveMaximum", constraints.lt());
-
-        // object constraints
-
-        if (constraints.minProperties() != -1)
-            object.put("minProperties", constraints.minProperties());
-        if (constraints.maxProperties() != -1)
-            object.put("maxProperties", constraints.maxProperties());
-
-        // array constraints
-
-        if (constraints.minItems() != -1)
-            object.put("minItems", constraints.minItems());
-        if (constraints.maxItems() != -1)
-            object.put("maxItems", constraints.maxItems());
-
-        if (constraints.uniqueItems())
-            object.put("uniqueItems", true);
-    }
-
-    protected static class StateData {
-        protected Map<Class<?>, JSONObject> references = new HashMap<>();
     }
 }
